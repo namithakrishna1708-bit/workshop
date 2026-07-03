74e05e7b9add36ba1b7499e779b8c109efba8f15
namithakrishna1708-bit <namithakrishna1708@gmail.com>
2026-07-03 10:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 #Take a userinput\
+st.title("take the input")
+
 name= st.text_input("Enter your name")
 
-st.title("take the input")
 
 if st.button("submit"):
   st.write(f"print the name: {name}")
